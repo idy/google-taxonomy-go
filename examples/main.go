0431aba7304e4b6211c2245f26a9d0406de12e43
idy <idy0013@gmail.com>
2017-04-23 11:54:23 +0800

    update examples

--- examples/main.go
+++ examples/main.go
@@ -6,19 +6,21 @@ import (
 	"github.com/xreception/google-taxonomy-go/taxonomy"
 )
 
+const keyLanguage = "en-US"
+
 func main() {
-	tx, err := taxonomy.NewTaxonomy([]string{})
+	tx, err := taxonomy.NewTaxonomy(keyLanguage, []string{})
 	if err != nil {
 		panic(err)
 	}
-	infs, err := tx.GetRootsCategoryInfo(taxonomy.KeyLanguage)
+	infs, err := tx.GetRootsCategoryInfo(keyLanguage)
 	if err != nil {
 		panic(err)
 	}
 	for _, inf := range infs {
 		fmt.Printf("%d - %s\n", inf.ID, inf.String())
 	}
-	inf, err := tx.GetCategoryInfo(16, taxonomy.KeyLanguage)
+	inf, err := tx.GetCategoryInfo(16, keyLanguage)
 	if err != nil {
 		panic(err)
 	}
